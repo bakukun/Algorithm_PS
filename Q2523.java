b437385fd86d59e258eec3644da4198f05213b27
bakukun <h1101201@gmail.com>
2021-10-12 00:21:46 +0900

    2021-10-11

---
+++ Q2523.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Q2523 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=0; i < n; i++){
+            for(int j=0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+        for(int i=0; i < n-1; i++){
+            for(int j=n-1; j > i; j--) {
+                System.out.print("*");
+            }
+            if(i == n-2){
+                break;
+            }
+            System.out.print("\n");
+        }
+    }
+}
+
---
+++ Q7287.java
@@ -0,0 +1,6 @@
+public class Q7287 {
+    public static void main(String[] args){
+        System.out.println("136");
+        System.out.println("bakukun");
+    }
+}
