8ea0fd084d12ac8076c56ccf3da20ac75c70e93c
bakukun <h1101201@gmail.com>
2021-10-17 01:40:26 +0900

    2021-10-16

--- Q1181.java
+++ Q1181.java
@@ -1,20 +1,38 @@
 import java.io.*;
 import java.util.*;
 
+
 public class Q1181 {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int n = Integer.parseInt(bf.readLine());
         String[] arr = new String[n];
-        int[][] arr2 = new int[2][13];
 
         for (int i = 0; i < n; i++) {
             arr[i] = bf.readLine();
-            arr2[0][i] = arr[i].length();
         }
 
-        Arrays.sort(arr2);
+        Arrays.sort(arr, new Comparator<String>(){ //정렬 기준 > 단어길이에 따라 분류 변경
+            public int compare(String s1,String s2){
+                if(s1.length() == s2.length()){
+                    return s1.compareTo(s2);
+                }
+                else{
+                    return s1.length() - s2.length();
+                }
+            }
+        });
 
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(arr[0]).append('\n');
+
+        for(int i=1; i < n;i++){
+            if(arr[i].equals(arr[i-1])==false){
+                sb.append(arr[i]).append('\n');
+            }
+        }
+
+        System.out.println(sb);
     }
 }
