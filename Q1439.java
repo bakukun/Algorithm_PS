0a6cf4e5c79fccdbe2a7b6fad1561f902411886b
bakukun <h1101201@gmail.com>
2021-10-19 01:38:55 +0900

    2021-10-17

--- Q1439.java
+++ Q1439.java
@@ -3,22 +3,40 @@ import java.io.*;
 public class Q1439 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		int sum = 0;
 		int cnt_0 = 0;
 		int cnt_1 = 0;
 		String str = bf.readLine();
 		int len = str.length();
-		while( (cnt_0 == 0 && cnt_1 != 0) || (cnt_1 == 0 && cnt_0 != 0) ){
-			for(int i =0;i<len;i++){
-				if(str.charAt(i)==48 ){
-
+		for(int i = 1;i<len;i++){
+			if(i == len-1) {
+				if (str.charAt(i - 1) == str.charAt(i)) {
+					if (str.charAt(i - 1) == 48) {
+						cnt_0++;
+					}
+					else {
+						cnt_1++;
+					}
 				}
-
+				else{
+					if(str.charAt(i-1)==48 &&str.charAt(i)==49){
+						cnt_0++;
+						cnt_1++;
+					}
+					else if(str.charAt(i-1)==49 &&str.charAt(i)==48){
+						cnt_1++;
+						cnt_0++;
+					}
+				}
 			}
-
+			else{
+				if(str.charAt(i-1)==48 &&str.charAt(i)==49){
+					cnt_0++;
+				}
+				else if(str.charAt(i-1)==49 &&str.charAt(i)==48){
+					cnt_1++;
+				}
+			}
 		}
-
-
+		System.out.println(cnt_0 > cnt_1 ? cnt_1 : cnt_0);
 	}
 }
