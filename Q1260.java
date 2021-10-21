d6f12540ab8754af4a8003afdf4c880be0efb171
bakukun <h1101201@gmail.com>
2021-10-22 00:05:11 +0900

    2021-10-21 unsolved

--- Q1260.java
+++ Q1260.java
@@ -1,60 +1,65 @@
 import java.util.*;
 
 public class Q1260 {
-	public static boolean[] visited = new boolean[9];
-	public static ArrayList<ArrayList<Integer>> graph = new ArrayList<ArrayList<Integer>>();
+    public static boolean[] visited = new boolean[1001];
+	public static boolean[] visited1 = new boolean[1001];
+    public static ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
 
 	// DFS 함수 정의
 	public static void dfs(int x) {
 		// 현재 노드를 방문 처리
 		visited[x] = true;
-		System.out.println(x + " ");
+		System.out.print(x + " ");
 		// 현재 노드와 연결된 다른 노드를 재귀적으로 방문
 		for (int i = 0; i < graph.get(x).size(); i++) {
 			int y = graph.get(x).get(i);
-			System.out.println("i :"+i+" x :"+x+" x size :"+graph.get(x).size()+" y :"+y);
 			if (!visited[y]) dfs(y);
 		}
 	}
+
+	public static void bfs(int start) {
+		Queue<Integer> q = new LinkedList<>();
+		q.offer(start);
+		// 현재 노드를 방문 처리
+		visited1[start] = true;
+		// 큐가 빌 때까지 반복
+		while(!q.isEmpty()) {
+			// 큐에서 하나의 원소를 뽑아 출력
+			int x = q.poll();
+			System.out.print(x + " ");
+			// 해당 원소와 연결된, 아직 방문하지 않은 원소들을 큐에 삽입
+			for(int i = 0; i < graph.get(x).size(); i++) {
+				int y = graph.get(x).get(i);
+				if(!visited1[y]) {
+					q.offer(y);
+					visited1[y] = true;
+				}
+			}
+		}
+	}
+
 	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    int n = sc.nextInt();
+	    int m = sc.nextInt();
+	    int start = sc.nextInt();
+
+
 		// 그래프 초기화
-		for (int i = 0; i < 9; i++) {
+		for (int i = 0; i <= n; i++) {
 			graph.add(new ArrayList<Integer>());
 		}
 
-			// 노드 1에 연결된 노드 정보 저장
-		graph.get(1).add(2);
-		graph.get(1).add(3);
-		graph.get(1).add(8);
-			// 노드 2에 연결된 노드 정보 저장
-		graph.get(2).add(1);
-		graph.get(2).add(7);
+		for(int i = 0; i < m; i++) {
+		    int a = sc.nextInt();
+		    int b = sc.nextInt();
+            graph.get(a).add(b);
+        }
 
-			// 노드 3에 연결된 노드 정보 저장
-		graph.get(3).add(1);
-		graph.get(3).add(4);
-		graph.get(3).add(5);
+		dfs(start);
+		System.out.print("\n");
+		bfs(start);
 
-			// 노드 4에 연결된 노드 정보 저장
-		graph.get(4).add(3);
-		graph.get(4).add(5);
-			// 노드 5에 연결된 노드 정보 저장
-		graph.get(5).add(3);
-		graph.get(5).add(4);
-
-			// 노드 6에 연결된 노드 정보 저장
-		graph.get(6).add(7);
-
-			// 노드 7에 연결된 노드 정보 저장
-		graph.get(7).add(2);
-		graph.get(7).add(6);
-		graph.get(7).add(8);
-
-			// 노드 8에 연결된 노드 정보 저장
-		graph.get(8).add(1);
-		graph.get(8).add(7);
-
-		dfs(1);
 	}
 }
 
