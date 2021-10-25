193b7205bc31024c7fe2f2e31b8b76413186b425
bakukun <h1101201@gmail.com>
2021-10-26 00:27:44 +0900

    2021-10-25

---
+++ Q2702.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Q2702 {
+    public static int gcd(int A, int B) {
+        if (B == 0) {
+            return A;
+        }
+        else{
+            return gcd(B, A%B);
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int cnt = Integer.parseInt(bf.readLine());
+        for(int i=0; i<cnt;i++){
+            StringTokenizer st = new StringTokenizer(bf.readLine(), " ");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            if(b>a){
+                int tmp = a;
+                a = b;
+                b = tmp;
+            }
+            int gcd = gcd(a,b);
+            bw.write(Integer.toString(a*b/gcd));
+            bw.write(" ");
+            bw.write(Integer.toString(gcd));
+            bw.newLine();
+        }
+        bw.flush();
+        bw.close();
+    }
+}
