04a36cf8b115c9e6f99847f646ca9349c9ede69d
JINSOO HONG <50573818+bakukun@users.noreply.github.com>
2024-10-01 01:19:39 +0900

    [level 3] Title: 네트워크, Time: 0.57 ms, Memory: 10.4 MB -BaekjoonHub

--- Python3/프로그래머스/3/43162. 네트워크/네트워크.py
+++ Python3/프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,3 +1,22 @@
 def solution(n, computers):
     answer = 0
+    visited = [False] * n
+    
+    def dfs(i):
+        #1. 해당 노드 방문 처리
+        #2. 해당 노드와 임의 인덱스 순차 비교
+        #3. 만약 값도 1이고 방문 안했다면 dfs
+        visited[i] = True
+        
+        for idx in range(n):
+            if(computers[i][idx] == 1 and not visited[idx]):
+                dfs(idx)
+                
+        return
+        
+    for i in range(n):
+        if (not visited[i]):
+            answer += 1
+            dfs(i)
+        
     return answer
