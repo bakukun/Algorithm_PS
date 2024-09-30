a5484a645d80be24241fc065a27e1d7e6d64fb00
JINSOO HONG <50573818+bakukun@users.noreply.github.com>
2024-09-30 17:02:00 +0900

    [level 2] Title: H-Index, Time: 2.94 ms, Memory: 10.4 MB -BaekjoonHub

--- Python3/프로그래머스/2/42747. H－Index/H－Index.py
+++ Python3/프로그래머스/2/42747. H－Index/H－Index.py
@@ -2,22 +2,11 @@ def solution(citations):
     
     n = len(citations)
     citations.sort()
-    maximum = max(citations)
     h = 0
-    
-    print(citations)
-    for i in range(maximum+1):
-        high_h = 0
-        under_h = 0 
-        for j in range(n):
-            
-            if (citations[j] < i):
-                under_h += 1
-            if (citations[j] >= i):
-                high_h += 1    
-        
-        print(i,high_h,under_h)
-        if (high_h >= i and under_h <= i):
-            h = i
+    for i in range(n):
+        print(i,citations[i],n-i)
+        if (citations[i] >= n-i):
+            h = max(h,n-i)
+            print("in",h)
     
     return h
