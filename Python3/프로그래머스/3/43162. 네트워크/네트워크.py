c8d07d5f5420b19a24e1c1c98cf869df334bbd6f
JINSOO HONG <50573818+bakukun@users.noreply.github.com>
2024-10-01 01:11:47 +0900

    [level 3] Title: 네트워크, Time: 0.55 ms, Memory: 10.3 MB -BaekjoonHub

--- Python3/프로그래머스/3/43162. 네트워크/네트워크.py
+++ Python3/프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,18 +1,3 @@
 def solution(n, computers):
-    cnt = 0
-    visited = [False] * n
-    
-    def dfs(idx):
-        
-        visited[idx] = True
-        
-        for connect_idx in range(n):
-            if computers[idx][connect_idx] == 1 and not visited[connect_idx]:
-                dfs(connect_idx)
-    
-    for i in range(n):
-        if not visited[i]:
-            cnt += 1
-            dfs(i)    
-                
-    return cnt
+    answer = 0
+    return answer
