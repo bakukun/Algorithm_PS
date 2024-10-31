547e3ddb655d025e241fb59b57721d82ce97389e
JINSOO HONG <50573818+bakukun@users.noreply.github.com>
2024-10-31 22:42:52 +0900

    [level 2] Title: 연속된 부분 수열의 합, Time: 1973.79 ms, Memory: 271 MB -BaekjoonHub

---
+++ Python3/프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.py
@@ -0,0 +1,22 @@
+def solution(sequence, k):
+    answer = []
+    start = 0
+    end = 0
+    add = 0
+
+    while start < len(sequence):
+        if add == k:
+            answer.append([start, end - 1])
+            add -= sequence[start]
+            start += 1
+        elif add > k:
+            add -= sequence[start]
+            start += 1
+        elif end < len(sequence):
+            add += sequence[end]
+            end += 1
+        else:
+            break
+    
+    answer.sort(key=lambda x: (x[1] - x[0], x[0]))
+    return answer[0]
