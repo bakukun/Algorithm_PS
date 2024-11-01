95055ad4dba25b1cc84e0c87d81c8d8d4618bed3
JINSOO HONG <50573818+bakukun@users.noreply.github.com>
2024-11-02 01:31:34 +0900

    [D2] Title: [S/W 문제해결 기본] 1일차 - 최빈수 구하기, Time: 127 ms, Memory: 47,692 KB -BaekjoonHub

---
+++ Python/SWEA/D2/1204. ［S／W 문제해결 기본］ 1일차 － 최빈수 구하기/［S／W 문제해결 기본］ 1일차 － 최빈수 구하기.py
@@ -0,0 +1,14 @@
+from collections import Counter
+
+T = int(input())
+# 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+for test_case in range(1, T + 1):
+    n = int(input())
+    arr = list(map(int, input().split()))
+    cnt_arr = Counter(arr)
+    
+    print(f"#{n}",cnt_arr.most_common(1)[0][0])
+    
+    
+    
+
